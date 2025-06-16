572814fe6d0d1db0457dabf868379a22571656ae
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-16 18:16:40 +0100

    Initial commit for luhn checksum program

---
+++ luhn_checksum/checksum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void findLastDigit(long long int number);
+
+int main(int argc, char *argv[]) {
+  long long int number = atoll(argv[1]);
+  // Check's that argc is exactly two and user input number i greater than zero
+  if (argc != 2) {
+    printf("Usage: %s <number>\n", argv[0]);
+    return 1;
+  }
+  if (number <= 0)
+    {
+      printf("Number you want to check must be positive integer.\n");
+      return 2;
+    }
+  findLastDigit(number);
+}
+
+// If two number sum is greater than 10
+void findLastDigit(long long int number) {
+  number %= 10;
+  printf("%lld\n", number);
+}
+
+
