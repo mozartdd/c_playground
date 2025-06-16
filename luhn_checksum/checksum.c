4dfecdff66ca9844020469641cad09f25fd954d1
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-16 19:34:39 +0100

    Added functionality to test if comand line argv[1] is valid luhn number

--- luhn_checksum/checksum.c
+++ luhn_checksum/checksum.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int getEverySecondDigit(long long int number, bool isSecond);
-// Looks if current digit of number is 2nd from end
-bool isSecondDigit = false;
+int getDigitSum(long long int number, bool isSecond);
 
 int main(int argc, char *argv[]) {
+  // Looks if current digit of number is 2nd from end
+  bool isSecondDigit = false;
   long long int number = atoll(argv[1]);
   // Check's that argc is exactly two and user input number i greater than zero
   if (argc != 2) {
@@ -18,16 +18,25 @@ int main(int argc, char *argv[]) {
       printf("Number you want to check must be positive integer.\n");
       return 2;
     }
+  // If number greater than max of long long int
   if (number > 922337203685477580) {
     printf("Please enter a smaller number\n");
     return 3;
   }
-  int secondDigitSum = getEverySecondDigit(number, isSecondDigit);
-  printf("%d\n", secondDigitSum);
+  int digitSum = getDigitSum(number, isSecondDigit);
+  int result = digitSum % 10;
+  if (result == 0) {
+    printf("Number: %lld is valid luhn number.\n", number);
+    return 0;
+  } else {
+    printf("Number: %lld is invalid luhn number.\n", number);
+    return 0;
+  }
 }
 
-// Finds and return last digit of int that need to be checked
-int getEverySecondDigit(long long int number, bool isSecond) {
+// Finds and return sum of every second digit
+// of int, starting with 2nd digit from right
+int getDigitSum(long long int number, bool isSecond) {
   int lastDigit;
   int sum = 0;
 
@@ -40,9 +49,13 @@ int getEverySecondDigit(long long int number, bool isSecond) {
       // If last digit multiplied by 2 is grater than 10, get it's
       // last digit of dividing it by reminder of 10 and add it to 1 
       if (lastDigit >= 10) {
-        lastDigit = 1 + (lastDigit % 10);
+        lastDigit -= 9;
       }
       // Sum of all last digit summed together
+      sum += lastDigit;
+    }
+    if (!isSecond) {
+      lastDigit = number % 10;
       sum += lastDigit;
     }
     number /= 10;
