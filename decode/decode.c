032724de1dd16c98d568aa6b5ee9149d82cfb9c8
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-25 18:40:37 +0100

    Added function to return reminders based on global flags

--- decode/decode.c
+++ decode/decode.c
@@ -1,9 +1,13 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void makeAlphabet(char array[]);
+int returnReminder(int number, bool value);
 void turnToIntegers(int arrayOfInts[], char *arrayOfString[], int length);
+bool isNumber = true;
+// 0 = uppercase, 1 = lowercase, 2 = symbol
+int current = 0;
 
 int main(int argc, char *argv[]) {
   if (argc < 4) {
@@ -12,24 +16,22 @@ int main(int argc, char *argv[]) {
   }
   // Array which holds alphabetical letters
   char alphabet[26];
+  // Dynamical memory allocation to hold array of array on integers
   int *integerArray = malloc((argc - 1) * sizeof(int));
   if (integerArray == NULL) {
     printf("Memory allocation failed\n");
-    return 1;
+    return 2;
   }
-  makeAlphabet(alphabet);
   turnToIntegers(integerArray, argv, argc);
+  int result;
 
   for (int i = 0; i < argc - 1; i++) {
-    printf("%d\n", integerArray[i]);
-  }  
-}
-
-// Creates alphabet
-void makeAlphabet(char array[]) {
-  for (int i = 0; i < 26; i++) {
-    array[i] = 'a' + i;
+    result = returnReminder(integerArray[i], isNumber);
+    printf("%d\n", result);
   }
+
+  free(integerArray);
+  return 0;
 }
 
 // Turns array of chars to array of integers
@@ -37,4 +39,20 @@ void turnToIntegers(int arrayOfInts[], char *arrayOfString[], int length) {
   for (int i = 0; i < length - 1; i++) {
     arrayOfInts[i] = atoi(arrayOfString[i + 1]);
   }
+}
+
+// Return reminder of letter divided by 26 or 8 and modifies global flags
+int returnReminder(int number, bool flag) {
+  int result = flag ? (number % 26) : (number % 8);
+  if (result == 0) {
+    current++;
+  }
+  if (current == 2) {
+    flag = false;
+  }
+  if (current == 3) {
+    current = 0;
+    flag = true;
+  }
+  return result;
 }
