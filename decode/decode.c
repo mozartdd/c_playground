73882f7c120b62013a236542dcd68ccc5d1ce9f0
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-25 19:33:03 +0100

    Completed cypher program for task from book "Think like a programmer"

--- decode/decode.c
+++ decode/decode.c
@@ -3,35 +3,37 @@
 #include <string.h>
 #include <stdlib.h>
 
-int returnReminder(int number);
-void turnToIntegers(int arrayOfInts[], char *arrayOfString[], int length);
-bool isNumber = true;
 // 0 = uppercase, 1 = lowercase, 2 = symbol
 int current = 0;
+bool isNumber = true;
+
+#define SYMBOL_COUNT 9
+#define LETTER_COUNT 27
+
+int returnReminder(int number);
+void turnToIntegers(int arrayOfInts[], char *arrayOfString[], int length);
+char returnPunctuation(int number);
 
 int main(int argc, char *argv[]) {
+  // Array of integers of dynamic length defined by argument count - 1
+  int integerArray[argc-1];
+  turnToIntegers(integerArray, argv, argc);
+
   if (argc < 4) {
     printf("Key must consist from at least 3 numbers\n");
     return 1;
   }
 
-  // Dynamical memory allocation to hold array of array on integers
-  int integerArray[argc-1];
-  if (integerArray == NULL) {
-    printf("Memory allocation failed\n");
-    return 2;
-  }
-  turnToIntegers(integerArray, argv, argc);
-  int result;
-
   for (int i = 0; i < argc - 1; i++) {
-    result = returnReminder(integerArray[i]);
+    int result = returnReminder(integerArray[i]);
     if (result == 0) {
-        printf("");
+        continue;
     } else if (current == 0) {
         printf("%c", result + 'A' - 1);
     } else if (current == 1) {
         printf("%c", result + 'a' - 1);
+    } else if (current == 2) {
+      printf("%c", returnPunctuation(result));
     }
   }
   printf("\n");
@@ -47,7 +49,7 @@ void turnToIntegers(int arrayOfInts[], char *arrayOfString[], int length) {
 
 // Return reminder of letter or symbol divided by 26 or 8 and modifies global flags
 int returnReminder(int number) {
-  int result = isNumber ? (number % 27) : (number % 9);
+  int result = isNumber ? (number % LETTER_COUNT) : (number % SYMBOL_COUNT);
   if (result == 0) {
     current++;
   }
@@ -59,4 +61,36 @@ int returnReminder(int number) {
     isNumber = true;
   }
   return result;
+}
+
+// Asign numeric value to symbol
+char returnPunctuation(int number) {
+  switch(number) {
+    case 1:
+    return '!';
+    break;
+  case 2:
+    return '?';
+    break;
+  case 3:
+    return ',';
+    break;
+  case 4:
+    return '.';
+    break;
+  case 5:
+    return ' ';
+    break;
+  case 6:
+    return ';';
+    break;
+  case 7:
+    return '"';
+    break;
+  case 8:
+    return '\'';
+    break;
+  default:
+    return '?';
+  }
 }
