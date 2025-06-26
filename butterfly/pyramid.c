a4850ae1865cda0ea71acf4abd784de80f0f4422
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-26 19:24:12 +0100

    C langugage practice and programing task completation

---
+++ butterfly/pyramid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int height = atoi(argv[1]);
+  if (argc != 2) {
+    printf("Usage: ./pyramid 'height'\n");
+    return 1;
+  } else if (height > 9 || height < 3) {
+    printf("Pyramid's height must be between 3 and 9.\n");
+    return 2;
+  }
+  for (int i = height; i > 0; i--) {
+    for (int s = i; s < height; s++) printf(" ");
+    for (int j = i; j > 0; j--) printf("##");
+    for (int s = i - height; s < height; s++) printf(" ");
+    printf("\n");
+  }
+}
