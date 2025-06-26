45442e3247c83453f37c6140b9fd9e16dd40f731
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-26 19:50:38 +0100

    Added functionality to program romb pyramid

--- butterfly/pyramid.c
+++ butterfly/pyramid.c
@@ -11,9 +11,15 @@ int main(int argc, char *argv[]) {
     return 2;
   }
   for (int i = height; i > 0; i--) {
+    for (int s = i - 1; s > 0; s--) printf(" ");
+    for (int j = i - 1; j < height; j++) printf("##");
+    for (int s = i - 1 ; s > 0; s--) printf(" ");
+    printf("\n");
+  }
+  for (int i = height; i > 0; i--) {
     for (int s = i; s < height; s++) printf(" ");
     for (int j = i; j > 0; j--) printf("##");
-    for (int s = i - height; s < height; s++) printf(" ");
+    for (int s = i; s < height; s++) printf(" ");
     printf("\n");
   }
 }
